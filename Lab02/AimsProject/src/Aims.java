48dc8ff1fe9375235f961c508d08e535851200c1
pdd04 <phanducduyhn@gmail.com>
2024-10-28 14:14:27 +0700

    lab02

---
+++ Lab02/AimsProject/src/Aims.java
@@ -0,0 +1,25 @@
+public class Aims {
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Howl's Moving Castle", "Animation", "Miyazaki Hayao", 159, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd4);
+
+        System.out.println("Total Cost is: " + anOrder.totalCost());
+
+        anOrder.displayCart();
+
+        anOrder.removeDigitalVideoDisc(dvd1);
+
+        System.out.println("Total Cost is: " + anOrder.totalCost());
+    }
+}
---
+++ Lab02/AimsProject/src/Cart.java
@@ -0,0 +1,57 @@
+public class Cart {
+
+    public static final int MAX_NUMBER_ORDERED = 20;
+    private static int qtyOrdered = 0;
+
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+
+    public int getQtyOrdered() {
+        return qtyOrdered;
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        this.qtyOrdered++;
+        if (this.qtyOrdered == MAX_NUMBER_ORDERED) {
+            System.out.println("The cart is almost full");
+        }else{
+            this.itemsOrdered[this.qtyOrdered - 1] = disc;
+            System.out.println("The disc has been added");
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        this.qtyOrdered--;
+        if (this.qtyOrdered == 0) {
+            System.out.println("The cart is empty");
+        }else{
+            this.itemsOrdered[this.qtyOrdered - 1] = null;
+            System.out.println("The disc has been removed");
+        }
+    }
+
+    public float totalCost() {
+        float total = 0;
+        for (int i = 0; i < this.qtyOrdered; i++) {
+            if(this.itemsOrdered[i] != null){
+                total += itemsOrdered[i].getCost();
+            }else{
+                break;
+            }
+        }
+        return total;
+    }
+
+    public void displayCart(){
+        for (int i = 0; i < this.qtyOrdered; i++) {
+            if(this.itemsOrdered[i] != null){
+                System.out.println("dvd" + (i + 1) + "{ ");
+                System.out.println("Title: " + itemsOrdered[i].getTitle() + ";");
+                System.out.println("Category: " + itemsOrdered[i].getCategory() + ";");
+                System.out.println("Director: " + itemsOrdered[i].getDirector() + ";");
+                System.out.println("Length: " + itemsOrdered[i].getLength() + ";");
+                System.out.println("Cost: " + itemsOrdered[i].getCost() + ";");
+                System.out.println("}");
+            }
+        }
+    }
+}
