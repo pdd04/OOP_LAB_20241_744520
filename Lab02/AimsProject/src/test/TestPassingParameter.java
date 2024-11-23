51ef819476ff4fe98ea635ed78bfbf9ee9204582
pdd04 <phanducduyhn@gmail.com>
2024-11-24 03:01:14 +0700

    TestPassingParameter

---
+++ Lab02/AimsProject/src/test/TestPassingParameter.java
@@ -0,0 +1,28 @@
+package test;
+
+import aims.disc.DigitalVideoDisc;
+
+public class TestPassingParameter {
+    public static void main(String[] args) {
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("Jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("Cinderella dvd title: " + cinderellaDVD.getTitle());
+
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("Jungle dvd title: " + jungleDVD.getTitle());
+    }
+    public static void swap(Object A, Object B) {
+        Object temp = A;
+        A = B;
+        B = temp;
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String newTitle) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(newTitle);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
+}
