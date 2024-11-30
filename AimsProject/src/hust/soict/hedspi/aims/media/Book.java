54507d26cefe2dad77ed00a461471df31fe62f4d
pdd04 <phanducduyhn@gmail.com>
2024-11-30 10:57:20 +0700

    refactor

--- AimsProject/src/hust/soict/hedspi/aims/media/Book.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/Book.java
@@ -3,51 +3,13 @@ package hust.soict.hedspi.aims.media;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Book {
-    private int id;
-    private String title;
-    private String category;
-    private double cost;
+public class Book extends Media {
+
     private List<String> author = new ArrayList<String>();
 
-    public Book(int id, String title, String category, double cost, List<String> author) {
-        this.id = id;
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
+    public Book(int id, String title, String category, float cost, List<String> author) {
+        super(id, title, category, cost);
         this.author = author;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public double getCost() {
-        return cost;
-    }
-
-    public void setCost(double cost) {
-        this.cost = cost;
     }
 
     public List<String> getAuthor() {
